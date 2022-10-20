f16793094f0e948af15a93788408d26f99b6e540
Katie067 <114251433+Katie067@users.noreply.github.com>
2022-10-20 10:02:19 -0700

    Add project files.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            notifyIcon1.Icon = SystemIcons.Application;
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Visible = !pictureBox1.Visible;
+        }
+
+        private void notifyIcon1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
